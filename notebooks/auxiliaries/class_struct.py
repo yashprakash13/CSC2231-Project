7395eda92f196aa51959b3e0aeb250f135698160
Siphelele <sipheleledanisa90@gmail.com>
2022-03-22 14:22:22 -0400

    Crude ResNet addition (yet to focus towards compression).

--- notebooks/auxiliaries/class_struct.py
+++ notebooks/auxiliaries/class_struct.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (
+    Activation,
+    Add,
+    BatchNormalization,
+    Conv2D,
+    Dense,
+    GlobalAveragePooling2D,
+    MaxPool2D,
+)
 
 
 class GAN(tf.keras.Model):
@@ -74,3 +84,45 @@ def gan_loss(logits, is_real=True):
     return tf.compat.v1.losses.sigmoid_cross_entropy(
         multi_class_labels=labels, logits=logits
     )
+
+
+class ResidualBlock(Model):
+    def __init__(self, channel_in=64, channel_out=256):
+        super().__init__()
+
+        channel = channel_out // 4
+
+        self.conv1 = Conv2D(channel, kernel_size=(1, 1), padding="same")
+        self.bn1 = BatchNormalization()
+        self.av1 = Activation(tf.nn.relu)
+        self.conv2 = Conv2D(channel, kernel_size=(3, 3), padding="same")
+        self.bn2 = BatchNormalization()
+        self.av2 = Activation(tf.nn.relu)
+        self.conv3 = Conv2D(channel_out, kernel_size=(1, 1), padding="same")
+        self.bn3 = BatchNormalization()
+        self.shortcut = self._shortcut(channel_in, channel_out)
+        self.add = Add()
+        self.av3 = Activation(tf.nn.relu)
+
+    def call(self, x):
+        h = self.conv1(x)
+        h = self.bn1(h)
+        h = self.av1(h)
+        h = self.conv2(h)
+        h = self.bn2(h)
+        h = self.av2(h)
+        h = self.conv3(h)
+        h = self.bn3(h)
+        shortcut = self.shortcut(x)
+        h = self.add([h, shortcut])
+        y = self.av3(h)
+        return y
+
+    def _shortcut(self, channel_in, channel_out):
+        if channel_in == channel_out:
+            return lambda x: x
+        else:
+            return self._projection(channel_out)
+
+    def _projection(self, channel_out):
+        return Conv2D(channel_out, kernel_size=(1, 1), padding="same")
